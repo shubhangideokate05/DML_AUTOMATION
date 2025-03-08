cba43dcacfa78b39f03454cd60bfa3359fc8b97f
Shubhangi Deokate <56465227+shubhangideokate05@users.noreply.github.com>
2025-03-08 18:29:37 +0530

    Update DMLQueries.sql

--- DMLQueries.sql
+++ DMLQueries.sql
@@ -1,8 +1,5 @@
 --These are the DML queries to be merged into main branch
 CREATE TABLE employees (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    age INT,
-    department VARCHAR(50),
-    salary DECIMAL(10,2)
+    id INT PRIMARY KEY,
+    name VARCHAR(100)
 );
